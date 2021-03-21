e794748a5aaec12e2482b36986528dbba16c2f8f
CdsDesarrolloIos <brian.barajas@alumnos.udg.mx>
2021-03-20 19:10:05 -0600

    Comentarios Añadidos y Capa de Constantes

---
+++ gaspiTestDev/New Group1/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  gaspiTestDev
+//
+//  Created by Brian Baragar on 20/03/21.
+//
+
+import Foundation
+struct Constants {
+    struct stringText {
+        static let findBar = "Buscar Producto"
+    }
+    struct nibNames {
+        static let productCell = "ProductViewCell"
+    }
+    struct imagesNames {
+        static let emptyImage =  "new-product"
+    }
+}
